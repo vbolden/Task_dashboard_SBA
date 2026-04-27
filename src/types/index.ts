a4befc3d202d7781ce12fab779a07eade657d693
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-27 15:30:16 -0400

    added interfaces for task and task form

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,18 @@
 export type Status = "pending" | "in-progress" | "done";
 export type Priority = "low" | "medium" | "high";
 
+export interface Task {
+    id: string;
+    title: string;
+    description: string;
+    status: Status;
+    priority: Priority;
+    dueDate?: string
+}
+
+export interface TaskFormProps {
+    title: string;
+    description: string;
+    priority: Priority;
+    dueDate?: string;
+}
