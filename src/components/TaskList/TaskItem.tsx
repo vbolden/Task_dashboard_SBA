b003a6a74ba631838fe9217812db1f1aa2246845
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-27 18:29:40 -0400

    added UI rendering for single task item

---
+++ src/components/TaskList/TaskItem.tsx
@@ -0,0 +1,31 @@
+import type { Task, Status, TaskItemProps } from "../../types";
+
+function TaskItem({task, onStatusChange, onDelete}: TaskItemProps) {
+    return (
+        <div>
+            <h3>{task.title}</h3>
+            <p>{task.description}</p>
+
+            <p>Priority: {task.priority}</p>
+
+            <select 
+            value={task.status}
+            onChange={(e) => 
+                onStatusChange(task.id, e.target.value as Status)
+            }
+            >
+                <option value="todo">Todo</option>
+                <option value="in-progress">In Progress</option>
+                <option value="done">Done</option>
+            </select>
+
+            <button
+            onClick={() => onDelete(task.id)}
+            >
+                Delete
+            </button>
+        </div>
+    );
+}
+
+export default TaskItem;
