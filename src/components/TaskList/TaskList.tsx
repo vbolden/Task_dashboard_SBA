c355f3c4d218db2a065ca7fbc543fec4581fc1fb
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-27 18:49:42 -0400

    added UI rendering for list

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,8 +1,23 @@
-import type { TaskListProps ,Status } from "../../types";
+import type { TaskListProps } from "../../types";
 import TaskItem from "./TaskItem";
 
-function TaskList() {
-    
+function TaskList({tasks, onStatusChange, onDelete}: TaskListProps) {
+    // UI IF THERE ARE NO TASKS IN LIST
+    if(tasks.length === 0) {
+        return <p>No Tasks Found.</p>
+    }
+
+    return (
+        <div>
+            {tasks.map(task => (
+                <TaskItem 
+                key={task.id}
+                task={task}
+                onStatusChange={onStatusChange}
+                onDelete={onDelete}/>
+            ))}
+        </div>
+    );
 }
 
 export default TaskList;
