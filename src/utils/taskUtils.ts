2786c41b18b04fdbc4b688b7846f4b61bc49bed3
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-27 16:42:53 -0400

    added function for filtering tasks by status, priority, or search and added function for sorting tasks

---
+++ src/utils/taskUtils.ts
@@ -0,0 +1,39 @@
+// ADD DATA LOGIC FOR FILTERING AND SEARCH
+import type { Task, TaskFilterProps } from "../types";
+
+// FUNCTION TO CHECK IF TASK STATUS, PRIORITY, OR SEARCH MATCHES SELECTED STATUS, FILTER, OR SEARCH
+export function filterTasks(tasks: Task[], filters: TaskFilterProps) {
+    // FIRST RETURNS FINAL FILTERED ARRAY
+    // LOOP THROUGH EACH TASK TO SEE IF STATUS MATCHES FILTER SELECTED, PRIORITY, OR SEARCH
+    return tasks.filter(task => {
+        // CONDITIONAL TO SHOW ALL IF NO FILTER IS SELECTED
+        const matchesStatus = 
+        !filters.status || task.status === filters.status;
+
+        // SAME FOR PRIORITY
+        const matchesPriority = 
+        !filters.priority || task.priority === filters.priority;
+
+        // SAME FOR SEARCH (CHANGE TO LOWER CASE FOR EASY MATCHING)
+        const matchesSearch = 
+        !filters.search || task.title.toLocaleLowerCase().includes(filters.search.toLocaleLowerCase());
+
+        // RETURNS VARIABLES TO TELL .FILTER() TO KEEP OR REMOVE TASK
+        return matchesStatus && matchesPriority && matchesSearch;
+    })
+}
+
+// FUNCTION FOR SORTING TASKS
+export function sortTasks(tasks: Task[], sortBy: string) {
+    // ADD IF ELSE STATEMENT TO HANDLE SORTING BY PRIORITY OR DATE
+    // ASSIGN EACH PRIORITY A NUMERIC VALUE TO SORT FROM HIGH TO LOW
+    
+    if(sortBy === "priority") {
+        const priorityOrder: Record<string, number> = {
+            
+        }
+        return [...tasks].sort((a, b) => 
+        a.priority.localeCompare(b.priority)
+    );
+    }
+}
