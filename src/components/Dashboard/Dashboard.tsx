9f4bc25bca6f996640982450b05df96ae9a30ade
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-27 20:57:14 -0400

    added imports, useStates, and component export

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,23 @@
 // STATE MANAGER COMPONENT 
 import { useState } from "react"
-import type { Task, TaskFilterProps } from "../../types"
+import type { Task, TaskFormData, Status, FilterOptions, SortOption } from "../../types"
+import TaskForm from "../TaskForm/TaskForm";
+import TaskList from "../TaskList/TaskList";
+import TaskFilter from "../TaskFilter/TaskFilter";
 
-// STATES TO MANAGE
-const [tasks, setTasks] = useState<Task[]>([]);
-const [filters, setFilters] = useState<TaskFilterProps>({});
-const [theme, setTheme] = useState<"light" | "dark">("dark");
+import { filterTasks, sortTasks } from "../../utils/taskUtils";
 
+function Dashboard() {
+    // STATES TO MANAGE
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [filters, setFilters] = useState<FilterOptions>({});
+    const [sortBy, setSortBy] = useState<SortOption>("");
+    // const [theme, setTheme] = useState<"light" | "dark">("dark");
+
+    return (
+
+)
+
+}
+
+export default Dashboard;
