265299d1fdab807dc279372e4b1f6093026906ab
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-27 21:12:18 -0400

    added functions to handle adding  tasks and to handle status change

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -11,12 +11,37 @@ function Dashboard() {
     // STATES TO MANAGE
     const [tasks, setTasks] = useState<Task[]>([]);
     const [filters, setFilters] = useState<FilterOptions>({});
-    const [sortBy, setSortBy] = useState<SortOption>("");
+    const [sortBy, setSortBy] = useState<SortOption>("date");
     // const [theme, setTheme] = useState<"light" | "dark">("dark");
+
+
+    // FUNCTION TO HANDLE ADDING TASK
+    const handleAddTask = (data: TaskFormData) => {
+        const newTask: Task = {
+            id: crypto.randomUUID(),
+            title: data.title,
+            description: data.description,
+            priority: data.priority,
+            dueDate: data.dueDate,
+            status: "todo",
+        };
+
+        // TAKE PREVIOUS TASKS ADDED IN ARRAY AND ADD NEW TASK TO THE ARRAY
+        setTasks((prev) => [...prev, newTask]);
+    }
+
+    // FUNCTION TO HANDLE STATUS CHANGE
+    const handleStatusChange = (id: string, status: Status) => {
+        setTasks((prev) =>
+            prev.map((task) =>
+                task.id === id ? { ...task, status } : task
+            )
+        );
+    };
 
     return (
 
-)
+    )
 
 }
 
