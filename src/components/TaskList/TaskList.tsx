b6b1d57e1c7bc1de1c5854d2679ec208e986d014
Valerie Bolden <valerie.bolden@gmail.com>
2026-07-05 14:28:45 -0400

    imported TaskList.css

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,18 +1,14 @@
 import type { TaskListProps } from "../../types";
 import TaskItem from "./TaskItem";
+import "../../TaskList.css"
 
 function TaskList({ tasks, onStatusChange, onDelete, onEdit }: TaskListProps) {
     // UI IF THERE ARE NO TASKS IN LIST
     if (tasks.length === 0) {
         return (
             <div className="empty-state">
-
                 <h2>No Tasks Yet</h2>
-
-                <p>
-                    Create your first task to get started.
-                </p>
-
+                <p>Create your first task to get started.</p>
             </div>
         );
     }
